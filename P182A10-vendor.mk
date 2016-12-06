fd3eb35762541c6217ba3a9182828044fe3dc956
GeorgeRamga <ramirez.garrido.j@gmail.com>
2016-12-06 02:43:47 -0600

    Add files via upload
    set config blade l2

---
+++ P182A10-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/zte/P182A10/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/zte/P182A10/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/zte/P182A10/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/zte/P182A10/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/zte/P182A10/xbin,system/xbin)
+
